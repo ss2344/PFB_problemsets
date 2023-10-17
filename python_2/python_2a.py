2730b37d74d8d24ee891de18e0255adefca850d6
pfb14 <pfb14@info14.cshl.edu>
2023-10-17 16:59:11 -0400

    adding python_2b.py

---
+++ python_2/python_2a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+count=30
+if count<0:
+	message= 'is negative'
+	print(count, message)
+elif count==0:
+	message='is zero'
+	print(count,message)
+elif count>0 and count<50:
+		if count<50 and count%2==0:
+			message='it is an even number that is smaller than 50'
+			print(count,message)
+elif count>50 and count%3==0:
+	message='it is larger than 50 and divisible by 3'
+	print(count,message)
+		
+else:
+	print('not matching')
