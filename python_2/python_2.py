17f7014cbb6f9af5282c4bebeca8265ec3f3076b
pfb14 <pfb14@info14.cshl.edu>
2023-10-17 15:36:32 -0400

    adding python_2.py

---
+++ python_2/python_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+my_variable='GTACCTTGATTTCGTATTCTGAGAGGCTGCTGCTTAGCGGTAGCCCCTTGGTTTCCGTGGCAACGGAAAA'
+if "Aye Captain" in my_variable:
+  print('Found')
+else:
+  print('not found')
